313eb5f18c5485a216c43d6588780ec2f28e32ec
xiangou.cao <xgcao@noitomrobotics.com>
2025-09-24 15:52:54 +0800

    update mocap to stickman ros2

--- mocap_to_stickman_ros2.py
+++ mocap_to_stickman_ros2.py
@@ -11,15 +11,18 @@ links_parent = {
     "RightUpLeg": "Hips",
     "RightLeg": "RightUpLeg",
     "RightFoot": "RightLeg",
+    "RightTiptoe": "RightFoot",
     "LeftUpLeg": "Hips",
     "LeftLeg": "LeftUpLeg",
     "LeftFoot": "LeftLeg",
+    "LeftTiptoe": "LeftFoot",
     "Spine": "Hips",
     "Spine1": "Spine",
     "Spine2": "Spine1",
     "Neck": "Spine2",
     "Neck1": "Neck",
     "Head": "Neck1",
+    "Head1": "Head",
     "RightShoulder": "Spine2",
     "RightArm": "RightShoulder",
     "RightForeArm": "RightArm",
@@ -40,7 +43,7 @@ links_parent = {
     "RightHandRing2": "RightHandRing1",
     "RightHandRing3": "RightHandRing2",
     "RightInHandPinky": "RightHand",
-    "RightHandPinky1": "RightInHandPinky",
+    "RightHandPinky1": "RightHandPinky",
     "RightHandPinky2": "RightHandPinky1",
     "RightHandPinky3": "RightHandPinky2",
     "LeftShoulder": "Spine2",
@@ -63,7 +66,7 @@ links_parent = {
     "LeftHandRing2": "LeftHandRing1",
     "LeftHandRing3": "LeftHandRing2",
     "LeftInHandPinky": "LeftHand",
-    "LeftHandPinky1": "LeftInHandPinky",
+    "LeftHandPinky1": "LeftHandPinky",
     "LeftHandPinky2": "LeftHandPinky1",
     "LeftHandPinky3": "LeftHandPinky2"
 }
@@ -72,8 +75,6 @@ def mocap_to_stickman_ros2():
     rclpy.init()
     node = Node("real_time_transform_publisher")
     node.create_timer(0.1, lambda: send_link_poses_tf(node))
-    json_file_path = './retarget.json'
-    robot = MCPRobot(open(json_file_path).read())
     app = MCPApplication()
     settings = MCPSettings()
     settings.set_udp(7012)
@@ -86,51 +87,8 @@ def mocap_to_stickman_ros2():
           evts = app.poll_next_event()
           for evt in evts:
               if evt.event_type == MCPEventType.AvatarUpdated:
-                  avatar = MCPAvatar(evt.event_data.avatar_handle)
-                  robot.update_robot(avatar)
-                  robot.run_robot_step()
-                  #print (robot.get_robot_ros_frame_json())
-                  
-                  # get realtime data
-                  real_time_data = json.loads(robot.get_robot_ros_frame_json()[0])
-
-                  # publish TF
-                  for link_name, pose_data in real_time_data["link_poses"].items():
-                        t = TransformStamped()
-                        t.header.stamp = node.get_clock().now().to_msg()
-                        t.header.frame_id =  links_parent[link_name]
-                        t.child_frame_id = link_name
-                        # translation
-                        t.transform.translation.x = pose_data[0]
-                        t.transform.translation.y = pose_data[1]
-                        t.transform.translation.z = pose_data[2]
-
-                        # rotation
-                        #quat = tf_transformations.quaternion_from_euler(0, 0, 0)
-                        t.transform.rotation.x = pose_data[3]
-                        t.transform.rotation.y = pose_data[4]
-                        t.transform.rotation.z = pose_data[5]
-                        t.transform.rotation.w = pose_data[6]
-                        br.sendTransform(t)
-                    
-                  # publish
-                  t = TransformStamped()
-                  t.header.stamp = node.get_clock().now().to_msg()
-                  t.header.frame_id = 'world'
-                  t.child_frame_id = 'base_link'
-
-                  # translation
-                  t.transform.translation.x = real_time_data["root_pos_x"]
-                  t.transform.translation.y = real_time_data["root_pos_y"]
-                  t.transform.translation.z = real_time_data["root_pos_z"]
-
-                  # rotation
-                  #quat = tf_transformations.quaternion_from_euler(0, 0, 0)
-                  t.transform.rotation.x = real_time_data["root_rot_x"]
-                  t.transform.rotation.y = real_time_data["root_rot_y"]
-                  t.transform.rotation.z = real_time_data["root_rot_z"]
-                  t.transform.rotation.w = real_time_data["root_rot_w"]
-                  br.sendTransform(t)
+                #   avatar = MCPAvatar(evt.event_data.avatar_handle)
+                  handleAvatar(br, node, evt.event_data.avatar_handle)
               
               elif evt.event_type == MCPEventType.RigidBodyUpdated:
                   print('rigid body updated')
@@ -143,6 +101,64 @@ def mocap_to_stickman_ros2():
       app.close()
       node.destroy_node()
       rclpy.shutdown()  
+
+def handleAvatar(br, node, avatar_handle):
+   
+        # Handle avatar update event
+        avatar = MCPAvatar(avatar_handle)  # Get avatar data
+
+        # Get all joint data
+        joints = avatar.get_joints()
+        for joint in joints:
+            link_name = joint.get_name()
+            position = joint.get_local_position()
+            rotation = joint.get_local_rotation()
+
+            # Create and send transform for each joint
+            send_transform(br, node, link_name, position, rotation)
+
+        # Head1 
+        rotation = (0.0, 0.0, 0.0, 1.0) 
+        position = (0.000000, 16.450001, 0.000000)
+        send_transform(br, node, 'Head1', position, rotation)
+
+        # LeftTiptoe RightTiptoe
+        position = (0.000000, -7.850000, 14.280000)
+        send_transform(br, node, 'LeftTiptoe', position, rotation)
+        send_transform(br, node, 'RightTiptoe', position, rotation)
+
+        # Handle the root joint of the robot
+        root_joint = avatar.get_root_joint()
+        position = root_joint.get_local_position()
+        rotation = root_joint.get_local_rotation()
+
+        # Publish the root joint transform
+        send_transform(br, node, 'base_link', position, rotation, frame_id='world')
+
+
+
+def send_transform(br, node, child_frame_id, position, rotation, frame_id=None):
+    
+        if frame_id is None:
+            frame_id = links_parent[child_frame_id]
+
+        t = TransformStamped()
+        t.header.stamp = node.get_clock().now().to_msg()
+        t.header.frame_id = frame_id
+        t.child_frame_id = child_frame_id
+
+        # Set translation
+        t.transform.translation.x = position[2] / 100
+        t.transform.translation.y = position[0] / 100
+        t.transform.translation.z = position[1] / 100
+
+        # Set rotation
+        t.transform.rotation.x = rotation[3]
+        t.transform.rotation.y = rotation[1]
+        t.transform.rotation.z = rotation[2]
+        t.transform.rotation.w = rotation[0]
+        # print(t.child_frame_id, t.transform.translation.x, t.transform.translation.y, t.transform.translation.z)
+        br.sendTransform(t)
 
 if __name__ == '__main__':
   mocap_to_stickman_ros2()
